5561fb6a54b617163fd0dd55a27fa917eb6f0388
lupyuen <luppy@appkaki.com>
2020-02-25 07:03:19 +0800

    Flipping coords

--- src/constants.rs
+++ src/constants.rs
@@ -8,12 +8,12 @@
 
 use cgmath::Vector2;
 
-pub const X_MIN: f64 = 0.0;
-pub const X_MAX: f64 = 32.0;
-pub const Y_MIN: f64 = 0.0;
-pub const Y_MAX: f64 = 16.0;
-pub const X_INCREMENT: f64 = 1.0;
-pub const Y_INCREMENT: f64 = 1.0;
+pub const X_PHYSICAL_MIN: f64 = 0.0;
+pub const X_PHYSICAL_MAX: f64 = 120.0;
+pub const Y_PHYSICAL_MIN: f64 = 0.0;
+pub const Y_PHYSICAL_MAX: f64 = 100.0;
+pub const X_PHYSICAL_INCREMENT: f64 = 1.0;
+pub const Y_PHYSICAL_INCREMENT: f64 = 1.0;
 
 //pub const SAMPLE_REGION: f64 = 3.5;
 //pub const FREQUENCY: f64 = 1.;
@@ -21,15 +21,15 @@ pub const Y_INCREMENT: f64 = 1.0;
 //pub const MAX_HEIGHT: f64 = 1.5;
 
 pub const GRID_SUBDIVISIONS: usize = 20; // 250;
-pub const OFFSET: f64 = -0.01;
-pub const GRID_WIDTH: f64 = (X_MAX - X_MIN) * 1.05;
-pub const GRID_HEIGHT: f64 = (Y_MAX - Y_MIN) * 1.05;
+pub const OFFSET: f64 = 0.0; // -0.01;
+pub const GRID_WIDTH: f64 = (X_PHYSICAL_MAX - X_PHYSICAL_MIN) * 1.05;
+pub const GRID_HEIGHT: f64 = (Y_PHYSICAL_MAX - Y_PHYSICAL_MIN) * 1.05;
 //pub const GRID_SIZE: f64 = SAMPLE_REGION * 1.05;
 pub const X_SCALE: f64 = 1.0 * GRID_WIDTH / (GRID_SUBDIVISIONS as f64);
 pub const Y_SCALE: f64 = 1.0 * GRID_HEIGHT / (GRID_SUBDIVISIONS as f64);
 //pub const SCALE: f64 = 1.0 * GRID_SIZE / (GRID_SUBDIVISIONS as f64);
 //pub const SCALE: f64 = 2.0 * GRID_SIZE / (GRID_SUBDIVISIONS as f64);
 pub const GRID_OFFSET: Vector2<f64> = Vector2 {
-    x: X_MIN, // GRID_SIZE,
-    y: Y_MIN, // GRID_SIZE,
+    x: X_PHYSICAL_MIN, // GRID_SIZE,
+    y: Y_PHYSICAL_MIN, // GRID_SIZE,
 };
